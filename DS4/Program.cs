217bce79aa71d76f54315b0dfa2240661f77267d
natasha <gromc-natalija@rambler.ru>
2022-10-04 22:49:55 +0300

    дз4

---
+++ DS4/Program.cs
@@ -0,0 +1,14 @@
+﻿int i, N;
+string userEnter;
+
+Console.Write ("Введите число N: ");
+userEnter = Console.ReadLine()!;
+N = int.Parse(userEnter);
+i=2;
+while (i<=N)
+{
+    Console.Write($"{i} ");
+    i=i+2;
+   
+}
+
