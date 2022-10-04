06c43e770e57e021010bc3c757749f7f59090a0e
natasha <gromc-natalija@rambler.ru>
2022-10-04 20:45:09 +0300

    дз2

---
+++ DS2/Program.cs
@@ -0,0 +1,32 @@
+﻿int a, b, c;
+string userEnter;
+
+Console.Write ("Введите число a: ");
+userEnter = Console.ReadLine()!;
+a = int.Parse(userEnter);
+
+Console.Write ("Введите число b: ");
+userEnter = Console.ReadLine()!;
+b = int.Parse(userEnter);
+
+Console.Write ("Введите число c: ");
+userEnter = Console.ReadLine()!;
+c = int.Parse(userEnter);
+
+if (a>b)
+{
+    if (a>c)
+    {
+        Console.Write ($"max = {a}",a);
+    }
+    else {Console.Write ($"max = {c}",c);}
+}
+
+else
+{
+    if (b>c)
+    {
+        Console.Write ($"max = {b}",b);
+    }
+    else {Console.Write ($"max = {c}",c);}
+}
