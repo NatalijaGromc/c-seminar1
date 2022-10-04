e004ad7c4ae0d3dd03ae4e0cb26f529fc36ba69c
natasha <gromc-natalija@rambler.ru>
2022-10-04 20:29:50 +0300

    дз1

---
+++ DS1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b;
+string userEnter;
+
+Console.Write ("Введите число a: ");
+userEnter = Console.ReadLine()!;
+a = int.Parse(userEnter);
+
+Console.Write ("Введите число b: ");
+userEnter = Console.ReadLine()!;
+b = int.Parse(userEnter);
+
+if (a>b)
+{
+    
+    Console.Write("max = ");
+    Console.Write(a);
+}
+
+else
+{
+    Console.Write("max = ");
+    Console.Write(b);
+}
