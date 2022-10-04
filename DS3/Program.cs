278e19e92069ef090dcbc79af8149befca7739df
natasha <gromc-natalija@rambler.ru>
2022-10-04 22:22:04 +0300

    дз3

---
+++ DS3/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+string userEnter;
+
+Console.Write ("Введите число a: ");
+userEnter = Console.ReadLine()!;
+a = int.Parse(userEnter);
+
+if (a%2==0)
+{
+    Console.Write($"Четное");
+   
+}
+
+else
+{
+    Console.Write($"Нечетное, остаток = {a%2}");
+}
